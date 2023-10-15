da233515cf7ba571eb4cc48e710e41f49b864451
Filip Duler <filip.duler@gmail.com>
2023-10-15 23:57:16 +0200

    feat: added header tests

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ TODO:
  - if an address causes too many errors stop processing their requests
  - remove old pending connections in update
  - set max outbound bandwidth
- - write header tests
  - write connection tests
  - send_broadcast to all on channel 
  - pinging and disconnecting clients
--- src/net/header.rs
+++ src/net/header.rs
@@ -134,3 +134,134 @@ impl Header {
         FRAG_HEADER_SIZE
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{ops::Deref, thread};
+
+    use crate::net::bytes;
+
+    use super::*;
+
+    #[test]
+    fn header_write_insufficient_size() {
+        let header = Header::new(0, 0, SendType::Reliable, false);
+        let mut buffer = vec![0_u8; header.get_header_size() - 1];
+        assert!(header
+            .write(&mut buffer, &mut IntBuffer::default())
+            .is_err());
+    }
+
+    #[test]
+    fn fragment_header_write_insufficient_size() {
+        let header = Header::new(0, 0, SendType::Reliable, true);
+        let mut buffer = vec![0_u8; header.get_header_size() - 1];
+        assert!(header
+            .write(&mut buffer, &mut IntBuffer::default())
+            .is_err());
+    }
+
+    #[test]
+    fn header_write() {
+        let mut header = Header::new(1, 2, SendType::Reliable, false);
+        header.ack = 3;
+        header.ack_bits = 4;
+
+        //offset it by 5 to test if the bound checks work
+        let mut int_buffer = IntBuffer::new_at(5);
+
+        let mut buffer = vec![0_u8; header.get_header_size() + 5];
+        assert!(header.write(&mut buffer, &mut IntBuffer::new_at(5)).is_ok());
+
+        int_buffer.goto(5);
+        assert_eq!(int_buffer.read_u16(&buffer), 1);
+        assert_eq!(
+            int_buffer.read_u8(&buffer),
+            PacketType::PayloadReliable as u8
+        );
+        assert_eq!(int_buffer.read_u64(&buffer), 2);
+        assert_eq!(int_buffer.read_u16(&buffer), 3);
+        assert_eq!(int_buffer.read_u32(&buffer), 4);
+    }
+
+    #[test]
+    fn fragment_header_write() {
+        let mut header = Header::new(1, 2, SendType::Reliable, true);
+        header.ack = 3;
+        header.ack_bits = 4;
+        header.fragment_group_id = 5;
+        header.fragment_id = 6;
+        header.fragment_size = 7;
+
+        //offset it by 5 to test if the bound checks work
+        let mut int_buffer = IntBuffer::new_at(5);
+
+        let mut buffer = vec![0_u8; header.get_header_size() + 5];
+        assert!(header.write(&mut buffer, &mut IntBuffer::new_at(5)).is_ok());
+
+        int_buffer.goto(5);
+        assert_eq!(int_buffer.read_u16(&buffer), 1);
+        assert_eq!(
+            int_buffer.read_u8(&buffer),
+            PacketType::PayloadReliableFrag as u8
+        );
+        assert_eq!(int_buffer.read_u64(&buffer), 2);
+        assert_eq!(int_buffer.read_u16(&buffer), 3);
+        assert_eq!(int_buffer.read_u32(&buffer), 4);
+        assert_eq!(int_buffer.read_u16(&buffer), 5);
+        assert_eq!(int_buffer.read_u8(&buffer), 6);
+        assert_eq!(int_buffer.read_u8(&buffer), 7);
+    }
+
+    #[test]
+    fn header_read_insufficient_size() {
+        let mut buffer = vec![0_u8; HEADER_SIZE - 1];
+        assert!(Header::read(&buffer).is_err());
+    }
+
+    #[test]
+    fn header_read() {
+        let mut header = Header::new(1, 2, SendType::Reliable, false);
+        header.ack = 3;
+        header.ack_bits = 4;
+
+        let mut buffer = vec![0_u8; header.get_header_size()];
+        assert!(header.write(&mut buffer, &mut IntBuffer::default()).is_ok());
+
+        let new_header = Header::read(&buffer);
+        assert!(new_header.is_ok());
+        let new_header = new_header.unwrap();
+
+        assert_eq!(header.seq, new_header.seq);
+        assert_eq!(header.packet_type, new_header.packet_type);
+        assert_eq!(header.session_key, new_header.session_key);
+        assert_eq!(header.ack, new_header.ack);
+        assert_eq!(header.ack_bits, new_header.ack_bits);
+    }
+
+    #[test]
+    fn fragmented_header_read() {
+        let mut header = Header::new(1, 2, SendType::Reliable, true);
+        header.ack = 3;
+        header.ack_bits = 4;
+        header.fragment_group_id = 5;
+        header.fragment_id = 6;
+        header.fragment_size = 7;
+
+        let mut buffer = vec![0_u8; header.get_header_size()];
+        assert!(header.write(&mut buffer, &mut IntBuffer::default()).is_ok());
+
+        let new_header = Header::read(&buffer);
+        assert!(new_header.is_ok());
+        let new_header = new_header.unwrap();
+
+        assert_eq!(header.seq, new_header.seq);
+        assert_eq!(header.packet_type, new_header.packet_type);
+        assert_eq!(header.session_key, new_header.session_key);
+        assert_eq!(header.ack, new_header.ack);
+        assert_eq!(header.ack_bits, new_header.ack_bits);
+        assert_eq!(header.fragment_group_id, new_header.fragment_group_id);
+        assert_eq!(header.fragment_id, new_header.fragment_id);
+        assert_eq!(header.fragment_size, new_header.fragment_size);
+    }
+}
